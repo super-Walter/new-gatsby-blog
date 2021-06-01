cfe247abd197d24bbee54936c8d70857f0e56a7f
Walter H <walter@walters-air.lan>
2021-06-01 12:01:34 -0700

    Add all code for new gatsby blog setup.

---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hello From The Third Page</h1>
+  </Layout>
+)
+
+export default ThirdPage
